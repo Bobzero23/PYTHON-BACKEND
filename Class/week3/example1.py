53076142dea1e8f45ab02a65cd5f16bf5d9b9555
Bobzero <rajabu.mustafa.00@gmail.com>
2023-03-13 12:03:45 +0300

    counter using inheritance and method overriding

--- Class/week3/example1.py
+++ Class/week3/example1.py
@@ -10,6 +10,15 @@ class Counter:
         self.count = self.count + 1
 
 
-object = Counter()
+class NewCounter(Counter):
+    def __init__(self, value):
+        Counter.__init__(self)
+        self.value = value
+
+    def increment(self):
+        self.count += self.value
+
+
+object = NewCounter(3)
 object.increment()
 print(object.count)
