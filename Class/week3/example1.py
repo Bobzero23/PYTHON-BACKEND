b6ab74ef3b443cd134782aef2b4042489ba2b786
Bobzero <rajabu.mustafa.00@gmail.com>
2023-03-13 12:06:00 +0300

    method to increment a variable by one

--- Class/week3/example1.py
+++ Class/week3/example1.py
@@ -1,8 +1,4 @@
 class Counter:
-    # def increment(self, value):
-    #     value = value + 1
-    #     return value
-
     def __init__(self):
         self.count = 0
 
@@ -10,15 +6,6 @@ class Counter:
         self.count = self.count + 1
 
 
-class NewCounter(Counter):
-    def __init__(self, value):
-        Counter.__init__(self)
-        self.value = value
-
-    def increment(self):
-        self.count += self.value
-
-
-object = NewCounter(3)
+object = Counter()
 object.increment()
 print(object.count)
